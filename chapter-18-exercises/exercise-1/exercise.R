d4336e64ab6c37230402b57b035f9c550044602d
徐枫雨 <FengyuXu@xufengyudeMacBook-Air.local>
2020-02-13 14:39:48 -0800

    ex18

--- chapter-18-exercises/exercise-1/exercise.R
+++ chapter-18-exercises/exercise-1/exercise.R
@@ -7,7 +7,9 @@ library("jsonlite")
 
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
+source("apikey.R")
 
+key_params <- list("api_key" = nyt_key)
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
 
